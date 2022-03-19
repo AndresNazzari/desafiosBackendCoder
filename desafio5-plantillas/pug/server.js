8136a52afc6918591c3284226bf890952216e231
stratocliff <lordcliff@gmail.com>
2022-03-19 20:33:47 -0300

    pug

---
+++ desafio5-plantillas/pug/server.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+const pug = require("pug")
+const PORT = process.env.PORT || 8080
+
+const ProductosAPI = require("./ProductosAPI")
+const productosAPI = new ProductosAPI()
+
+// Config para que express reconozca el body de una solicitud post
+// si no pongo esto no puede reconocer el req.body
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//Middleware de nivel de aplicacion
+//La función se ejecuta cada vez que la aplicación recibe una solicitud.
+// app.use(function (req, res, next) {
+//   console.log('Time:', Date.now());
+//   next();
+// });
+
+//Configuracion de Handlebars
+// app.engine("hbs", handlebars.engine({
+//     extname: ".hbs",
+//     defaultLayout: "index.hbs",
+//     layoutDir: __dirname + "/views/layouts",
+//     partialDir: __dirname+ "/views/partials"
+// }))
+app.set("view engine", "pug")
+app.set("views", "./views")
+
+
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+})
+server.on("error", error => console.log(`Error en servidor ${error}`))
+
+//Definir rutas 
+// app.use('/api/productos', require('./routes/api/productos'))
+
+app.get('/productos', (req, res) => {
+    const productos = productosAPI.getAll()
+    productos.length > 0 ?
+        res.render("productos.pug", {productos})  :
+        res.render("productos.pug", { msg: "No hay productos" }) 
+})
+ 
+app.post('/productos', (req, res) => {
+    productosAPI.save(req.body)
+    res.redirect("/productos")
+})
+
+
+app.use(/* "/static",  */express.static(__dirname + "/public"))
