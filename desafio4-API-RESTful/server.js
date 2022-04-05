92166ffb10de8b8685ad1aa65dabbfb221809e5e
unknown <andresnazzari@gmail.com>
2022-04-05 12:57:26 +0900

    prueba

--- desafio4-API-RESTful/server.js
+++ desafio4-API-RESTful/server.js
@@ -1,28 +1,27 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 8080
-
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 // Config para que express reconozca el body de una solicitud post
 // si no pongo esto no puede reconocer el req.body
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Middleware de nivel de aplicacion
 //La función se ejecuta cada vez que la aplicación recibe una solicitud.
 app.use(function (req, res, next) {
-  console.log('Time:', Date.now());
-  next();
+    console.log('Time:', Date.now());
+    next();
 });
 
-
 const server = app.listen(PORT, () => {
-    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
-})
-server.on("error", error => console.log(`Error en servidor ${error}`))
+    console.log(
+        `Servidor http escuchando en el puerto ${server.address().port}`
+    );
+});
+server.on('error', (error) => console.log(`Error en servidor ${error}`));
 
-//Definir rutas 
-app.use('/api/productos', require('./routes/api/productos'))
+//Definir rutas
+app.use('/api/productos', require('./routes/api/productos'));
 
-
-app.use("/static", express.static(__dirname+"/public"))
+app.use('/static', express.static(__dirname + '/public'));
