ecd87aba9cd824d9ad50373931b5dd4b48edd1b0
Andres Nazzari <lordcliff@gmail.com>
2022-07-05 17:28:58 +0900

    🚑 HOTFIX: twilio number

--- proyectoFinal-3erEntrega/middleware/twilio.js
+++ proyectoFinal-3erEntrega/middleware/twilio.js
@@ -7,14 +7,15 @@ const sendWA = async (user, cart) => {
     const numbers = [config.TWILIO_PHONE, user.telefono];
     //numbers es igual a [ ' +1 864 668 3602','+541167413192' ]
 
-    numbers.forEach(async (number) => {
-        const message = await client.messages.create({
-            body: `Nueva compra de ${user.nombre}, con los siguientes items ${cart}`,
-            from: `whatsapp:${config.TWILIO_PHONE}`,
-            to: `whatsapp:${number}`,
-        });
-        console.log(message.sid);
-    });
+    //lo comento ya que no me funciona debido a que le di a release al numero de twilio y para poner uno nuevo me cobran
+    // numbers.forEach(async (number) => {
+    //     const message = await client.messages.create({
+    //         body: `Nueva compra de ${user.nombre}, con los siguientes items ${cart}`,
+    //         from: `whatsapp:${config.TWILIO_PHONE}`,
+    //         to: `whatsapp:${number}`,
+    //     });
+    //     console.log(message.sid);
+    // });
 };
 
 module.exports = { sendWA };
